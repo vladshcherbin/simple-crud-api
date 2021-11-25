aa2e6e1335901b42f1d869c1793e7d71d51f7b43
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-11-25 13:08:26 +0300

    Add random persons data

---
+++ src/data/persons.js
@@ -0,0 +1,26 @@
+import { randomInt, randomUUID } from 'crypto'
+
+function generateName() {
+  const names = ['Alex', 'Sam', 'Susan', 'Bob', 'James', 'Sarah']
+
+  return names[randomInt(names.length - 1)]
+}
+
+function generateAge() {
+  return randomInt(6, 60)
+}
+
+function generateHobbies() {
+  const hobbies = ['tennis', 'games', 'singing', 'dancing', 'hiking']
+
+  return Array.from({ length: randomInt(3) }, () => (
+    hobbies[randomInt(hobbies.length - 1)]
+  ))
+}
+
+export default Array.from({ length: randomInt(2, 4) }, () => ({
+  id: randomUUID(),
+  name: generateName(),
+  age: generateAge(),
+  hobbies: generateHobbies()
+}))
