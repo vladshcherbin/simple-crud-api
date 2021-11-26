73768900faec008fe09d5cf6edf2c5cfca286252
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-11-26 12:17:06 +0300

    Add persons list mock

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,19 +1,35 @@
+import { jest } from '@jest/globals'
 import request from 'supertest'
-import server from '../src/server'
+
+jest.unstable_mockModule('../src/data/persons', () => ({
+  default: [
+    {
+      id: '02d2de0d-28f6-4d9a-b2cd-d9725b8d16c1',
+      name: 'Alex',
+      age: 18,
+      hobbies: ['tennis']
+    },
+    {
+      id: '75bae368-133e-4896-9655-19ddf027ad14',
+      name: 'Sam',
+      age: 6,
+      hobbies: []
+    },
+    {
+      id: 'b73a288b-7a09-4fa6-b988-2b14235e1e95',
+      name: 'Susan',
+      age: 41,
+      hobbies: ['dancing', 'hiking']
+    }
+  ]
+}))
+
+const { default: server } = await import('../src/server')
 
 test('Return persons list', async () => {
   const response = await request(server).get('/')
 
   expect(response.status).toBe(200)
   expect(response.type).toBe('application/json')
-  expect(response.body).toEqual(expect.any(Array))
-
-  response.body.forEach((element) => {
-    expect(element).toEqual(expect.objectContaining({
-      id: expect.any(String),
-      name: expect.any(String),
-      age: expect.any(Number),
-      hobbies: expect.any(Array)
-    }))
-  })
+  expect(response.body).toMatchSnapshot()
 })
