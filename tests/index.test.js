4dfaa83387bcbf75fc73d035ea725ae0c6fb50de
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-11-27 17:43:11 +0300

    Add crypto mock, add method to test name

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,33 +1,46 @@
+import crypto from 'crypto'
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { jest } from '@jest/globals'
 import request from 'supertest'
 
+const mockedPersons = [
+  {
+    id: '02d2de0d-28f6-4d9a-b2cd-d9725b8d16c1',
+    name: 'Alex',
+    age: 18,
+    hobbies: ['tennis']
+  },
+  {
+    id: '75bae368-133e-4896-9655-19ddf027ad14',
+    name: 'Sam',
+    age: 6,
+    hobbies: []
+  },
+  {
+    id: 'b73a288b-7a09-4fa6-b988-2b14235e1e95',
+    name: 'Susan',
+    age: 41,
+    hobbies: ['dancing', 'hiking']
+  }
+]
+
+const mockedRandomUUID = jest.fn(() => crypto.randomUUID())
+
+jest.unstable_mockModule('crypto', () => ({
+  default: {
+    ...crypto,
+    randomUUID: mockedRandomUUID
+  }
+}))
+
 jest.unstable_mockModule('../src/data/persons', () => ({
-  default: [
-    {
-      id: '02d2de0d-28f6-4d9a-b2cd-d9725b8d16c1',
-      name: 'Alex',
-      age: 18,
-      hobbies: ['tennis']
-    },
-    {
-      id: '75bae368-133e-4896-9655-19ddf027ad14',
-      name: 'Sam',
-      age: 6,
-      hobbies: []
-    },
-    {
-      id: 'b73a288b-7a09-4fa6-b988-2b14235e1e95',
-      name: 'Susan',
-      age: 41,
-      hobbies: ['dancing', 'hiking']
-    }
-  ]
+  default: mockedPersons
 }))
 
 const { default: server } = await import('../src/server')
 
 describe('Routes', () => {
-  describe('/person', () => {
+  describe('GET /person', () => {
     test('Return persons list', async () => {
       const response = await request(server.instance()).get('/person')
 
