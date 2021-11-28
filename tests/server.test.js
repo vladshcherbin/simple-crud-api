9713af46e1c09202d290ea82bc983048858d0c7a
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-11-28 22:36:47 +0300

    Add invalid json test

--- tests/server.test.js
+++ tests/server.test.js
@@ -16,3 +16,11 @@ test('Return 500 when route handler is missing', async () => {
   expect(response.type).toBe('application/json')
   expect(response.body).toMatchSnapshot()
 })
+
+test('Return 400 request body is invalid', async () => {
+  const response = await request(server.instance()).post('/person').send('{')
+
+  expect(response.status).toBe(400)
+  expect(response.type).toBe('application/json')
+  expect(response.body).toMatchSnapshot()
+})
